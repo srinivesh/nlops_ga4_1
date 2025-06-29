444959e7cc4ad788f6fa2480c6ae3fb02779e71b
srinivesh <kernelguy+discord@gmail.com>
2025-06-29 15:16:24 +0000

    testing push

--- iris_knn.py
+++ iris_knn.py
@@ -26,7 +26,7 @@ y = data[:, 4]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Create a K-Nearest Neighbors (KNN) classifier
-knn = KNeighborsClassifier(n_neighbors=5)
+knn = KNeighborsClassifier(n_neighbors=3)
 
 # Train the model on the training data
 knn.fit(X_train, y_train)
